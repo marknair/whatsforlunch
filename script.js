a5c70e4cf337767a4aa781082dba44bec1a76990
Mark Nair <47827647+marknair@users.noreply.github.com>
2025-01-21 22:05:59 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// TODO: Create your arrays here
+let foodTypes = [
+    // Add different types of food
+]
+
+let restaurants = [
+    // Add restaurants
+]
+
+
+function decideLunch() {
+    // TODO: Write your code here
+    // 1. Pick a random food type
+    // 2. Pick a matching restaurant
+    // 3. Display the result
+    // For example, the result should say something like "Today you should eat linguini at Pizza Nomad!"
+}
